6662b641afe2bd65deb630166c3afa1785fddbf5
Kevin Castillo Mora <kecoco16@gmail.com>
2018-09-26 17:54:25 -0600

    Authorization enable cors

--- src/api/app.js
+++ src/api/app.js
@@ -19,7 +19,6 @@ const router = asyncify(express.Router())
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*')
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization')
-  
   next()
 })
 
